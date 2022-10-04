9e7bced877ef0fc3d2fa63ef7f246997dbecd61e
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-10-04 12:38:01 -0300

    finalizamos maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AdminColores from "./components/AdminColores";
+import "./app.css"
 
 function App() {
   return (
--- src/components/AdminColores.jsx
+++ src/components/AdminColores.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
 const AdminColores = () => {
     return (
-   <div>
-  <Form>
-      <Form.Group className="m-5" >
+   <div className="contenedor">
+  <Form >
+    <h1>Administrar colores</h1>
+      <Form.Group >
         <Form.Label>Elija un color</Form.Label>
         <Form.Control type="text" placeholder="por ej: naranja" />
      </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" className="btn">
         Enviar
       </Button>
     </Form>
