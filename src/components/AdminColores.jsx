624304bad4e88b306932310b6f541ce454695e5c
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-10-10 21:49:03 -0300

    agregamos tarea y limpiamos le value

--- src/components/AdminColores.jsx
+++ src/components/AdminColores.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 import Colores from "./Colores";
 
 const AdminColores = () => {
+  // aqui va la logica
+  const [color, setColor] = useState("");
+  const [arrayColor, setArrayColor] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArrayColor([...arrayColor, color]);
+    setColor("")
+  };
 
   return (
     <Container>
-    <Form className="formulario">
-  <Form.Group className ="titulo">Administrar Colores</Form.Group>
-    <Form.Group className="mb-3 elegirColor" >
-      <Form.Label>Elija un color</Form.Label>
-      <Form.Control type="text" placeholder="Por ej: Naranja"/>
-    </Form.Group>
-    <Button variant="warning" type="submit" className="boton">
-      Enviar
-    </Button>
-  </Form>
-  <Colores></Colores>
-  </Container>
- 
+      <Form className="formulario" onSubmit={handleSubmit}>
+        <Form.Group className="titulo">Administrar Colores</Form.Group>
+        <Form.Group className="mb-3 elegirColor">
+          <Form.Label>Elija un color</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Por ej: Naranja"
+            onChange={(e) => setColor(e.target.value)}
+            value={color}
+          />
+        </Form.Group>
+        <Button variant="warning" type="submit" className="boton">
+          Enviar
+        </Button>
+      </Form>
+      <Colores></Colores>
+    </Container>
   );
 };
-
 
 export default AdminColores;
